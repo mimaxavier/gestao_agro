436657ca8b8b46d3d34604cc440eec6e537bfbf9
Jemima Larissa Ferreira Xavier dos Reis <jemimaxavier@MacBook-Air-de-Jemima.local>
2026-05-28 12:35:43 -0300

    feature/vaccine

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 from models.animal import Animal
 from models.plantation import Plantation
+from models.vaccine import VaccineApplication
 
-corn = Plantation(1, "Corn", "24/01/2026")
+'''corn = Plantation(1, "Corn", "24/01/2026")
 
 print(corn.expected_harvest_date)
 
 Soybean = Plantation(2, "Soybean", "22/05/2026")
 
-print(Soybean.expected_harvest_date)
+print(Soybean.expected_harvest_date)'''
+
+aplicacao1 = VaccineApplication(12, "Brucelose", "23/04/2026")
+
+print(aplicacao1.next_dose)
+print(aplicacao1.status)
+
 
---
+++ models/vaccine.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
+
+class VaccineApplication:
+    def __init__(self, animal_id, vaccine_name, apply_date):
+        self.animal_id = animal_id
+        self.vaccine_name = vaccine_name
+        self.apply_date = datetime.strptime(apply_date, r"%d/%m/%Y")
+        self.next_dose = self.calculate_next_dose()
+        self.status = self.display_info()
+
+
+    def calculate_next_dose(self):
+        interval_days = {
+            "Brucelose": 100,
+            "Raiva": 150,
+        }
+
+        interval = interval_days[self.vaccine_name]
+
+        next_dose = self.apply_date + timedelta(days=interval)
+
+        return next_dose
+
+
+    def is_overdue(self):
+       return date.today() > self.next_dose
+
+    def display_info(self):
+        dataformatada = self.apply_date.strftime("")
+
+        status = { "Animal": self.animal_id,
+            "Última Vacina": self.vaccine_name,
+            "Data da Última vacina": self.apply_date,
+            "Data da Próxima dose": self.next_dose
+        }
+
+        return status
+    
+    def add_notes(self, note):
+        pass
+    
+    def validate_apply_date(self):
+        pass
+
+    def register_booster(self):
+        pass
