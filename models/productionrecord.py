20db384ccec7be95abb667e3438bb7ec7904ba9e
Jemima Xavier <mimaxavier01@gmail.com>
2026-06-02 18:37:19 -0300

    Create ProductionRecord class

--- models/productionrecord.py
+++ models/productionrecord.py
@@ -1,2 +1,5 @@
 class ProductionRecord:
-    pass
+    def __init__(self, production_type, date_production):
+        self.production_type = production_type
+        self.date_production = date_production
+        
