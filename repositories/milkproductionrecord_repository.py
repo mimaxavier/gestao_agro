c00855e7a3db36d045daf068c533e7de0bf35209
Jemima Reis <82865302+mimaxavier@users.noreply.github.com>
2026-06-22 17:08:34 -0300

    Atualizar o milkproductionrecord_repository.py

---
+++ repositories/milkproductionrecord_repository.py
@@ -0,0 +1,56 @@
+class MilkProductionRecord:
+
+    def save(self, animal):
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute()
+
+        conn.commit()
+        conn.close()
+
+    def get_by_id(self, animal_id: int):
+        query = "SELECT especie, birth_date, weight, id FROM animals WHERE id = ?"
+
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute(query, (animal_id,))
+        resultado = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+        if not resultado:
+            return None
+        
+        converted_date = date.fromisoformat(resultado[1]) if resultado[1] else None
+
+        return Animal(
+            id=resultado[3],
+            especie = resultado[0],
+            birth_date = converted_date,
+            weight = resultado[2],
+        )
+
+    def update(self, animal):
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute()
+
+        conn.commit()
+        conn.close()
+
+    def delete(self, animal_id):
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute()
+
+        conn.commit()
+        conn.close()
