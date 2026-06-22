0300a069e5bfdaf21b8f89f2be1bcc7a0bced1aa
Jemima Reis <82865302+mimaxavier@users.noreply.github.com>
2026-06-22 17:07:14 -0300

    Atualizar o feedingrecord_repository.py

--- repositories/feedingrecord_repository.py
+++ repositories/feedingrecord_repository.py
@@ -1,7 +1,56 @@
-
 class FeedingRecord:
-    
+
     def save(self, animal):
-        pass
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute()
+
+        conn.commit()
+        conn.close()
+
+    def get_by_id(self, animal_id: int):
+        query = "SELECT especie, birth_date, weight, id FROM animals WHERE id = ?"
+
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute(query, (animal_id,))
+        resultado = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+        if not resultado:
+            return None
         
-    def 
+        converted_date = date.fromisoformat(resultado[1]) if resultado[1] else None
+
+        return Animal(
+            id=resultado[3],
+            especie = resultado[0],
+            birth_date = converted_date,
+            weight = resultado[2],
+        )
+
+    def update(self, animal):
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute()
+
+        conn.commit()
+        conn.close()
+
+    def delete(self, animal_id):
+        conn = sqlite3.connect("database/farm.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute()
+
+        conn.commit()
+        conn.close()
